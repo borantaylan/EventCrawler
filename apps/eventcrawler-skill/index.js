64c484d780a4c89a87b07fbf032f3d5e10d18f01
borantaylan <borantaylan@hotmail.com>
2017-12-11 16:40:40 +0100

    skill implementation

---
+++ apps/eventcrawler-skill/index.js
@@ -0,0 +1,34 @@
+module.change_code = 1;
+'use strict';
+
+var alexa = require( 'alexa-app' );
+var app = new alexa.app( 'eventcrawler-skill' );
+
+
+app.launch( function( request, response ) {
+	response.say( 'Welcome to eventcrawler!' ).shouldEndSession( false );
+} );
+
+
+app.error = function( exception, request, response ) {
+	console.log(exception)
+	console.log(request);
+	console.log(response);	
+	response.say( 'Sorry an error occured ' + error.message);
+};
+
+app.intent('event',
+  {
+    "slots":{"date":"AMAZON.DATE","locality":"LIST_OF_LOCALITIES"}
+	,"utterances":[ 
+		"What events are there in {locality} for {date}"
+		]
+  },
+  function(request,response) {
+    var date = request.slot('date');
+    var locality = request.slot('locality');
+    response.say("You asked for "+date + " " + locality);
+  }
+);
+
+module.exports = app;
