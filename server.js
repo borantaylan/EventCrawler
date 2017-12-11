47ccc238d185a632423f3a106c31d9d49d0cfea6
borantaylan <borantaylan@hotmail.com>
2017-12-11 15:56:12 +0100

    Second commit: server.js and Procfile is added. NPM is initiated.

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var AlexaAppServer = require( 'alexa-app-server' );
+
+var server = new AlexaAppServer( {
+	httpsEnabled: false,
+	port: process.env.PORT || 7600
+} );
+
+server.start();
